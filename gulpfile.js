8c79a7af29851b67de145504fb1c98f31e1257cd
Vincent Prouillet <vincent@wearewizards.io>
2015-05-10 20:29:14 +0100

    Update packages and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,28 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
-var prefix = require('gulp-autoprefixer');
+var autoprefixer = require('gulp-autoprefixer');
 
+var sassFiles = [
+  './*.scss',
+  './base/*.scss',
+  './components/*.scss'
+];
 
-var sassFiles = ['./*.scss', './base/*.scss', './components/*.scss'];
-
-
-gulp.task('browser-sync', function () {
-  browserSync.init(null, {
-    open: false,
-    server: {
-      baseDir: "./"
-    },
-    watchOptions: {
-      debounceDelay: 1000
-    }
-  });
-});
 
 gulp.task('sass', function() {
-    gulp.src(sassFiles)
-        .pipe(sass({outputStyle: 'compressed', errLogToConsole: true}))
-        .pipe(prefix())  // defauls to > 1%, last 2 versions, Firefox ESR, Opera 12.1
-        .pipe(gulp.dest('./'));
+  return gulp.src(sassFiles)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('watch', ['sass', 'browser-sync'], function () {
-    gulp.watch(sassFiles, ['sass']);
+gulp.task('watch', function() {
+  browserSync({
+    open: false,
+    server: "./"
+  });
 
-    gulp.watch('./sink.css', function(file) {
-      if (file.type === "changed") {
-        return browserSync.reload(file.path);
-      }
-    });
+  gulp.watch(sassFiles, gulp.series('sass'));
 });
